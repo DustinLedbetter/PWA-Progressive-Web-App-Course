ac03b824e36c867fd3e680d45b947f7a440f5a06
Dustin Ledbetter <dustinledbetter9@gmail.com>
2020-02-14 11:41:51 -0500

    Add files via upload

---
+++ PWA Progressive Web App/js/app.js
@@ -0,0 +1,46 @@
+// Select the container for cards
+const container = document.querySelector(".container")
+
+// Array of cards with names and images
+const coffees = [
+  { name: "Perspiciatis", image: "images/coffee1.jpg" },
+  { name: "Voluptatem", image: "images/coffee2.jpg" },
+  { name: "Explicabo", image: "images/coffee3.jpg" },
+  { name: "Rchitecto", image: "images/coffee4.jpg" },
+  { name: " Beatae", image: "images/coffee5.jpg" },
+  { name: " Vitae", image: "images/coffee6.jpg" },
+  { name: "Inventore", image: "images/coffee7.jpg" },
+  { name: "Veritatis", image: "images/coffee8.jpg" },
+  { name: "Accusantium", image: "images/coffee9.jpg" },
+]
+
+// With this code, we can now loop through the array and show them on the HTML file
+const showCoffees = () => {
+  let output = ""
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+}
+
+// And to make everything work, we wait until the DOM (Document Object Model) content finishes loading to run the showCoffees method
+document.addEventListener("DOMContentLoaded", showCoffees)
+
+
+// Here, we start by checking if the serviceWorker is supported by the current browser (as it's still not supported by all browsers).
+// Then, we listen to the page load event to register our service worker by passing the name of our file serviceWorker.js to navigator.serviceWorker.register() as a parameter to register our worker.
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
